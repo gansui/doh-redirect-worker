d7614ab961ed2d0328130bc054fcc3be91f8b8a5
gansui <138343088+gansui@users.noreply.github.com>
2024-11-05 10:07:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,14 +6,17 @@ export default {
   const url = new URL(request.url)
   const { pathname, search } = url
 
-  if (pathname == "/") {
-    return Response.redirect("https://milgradesec.github.io/paesadns/", 301)
+  if (pathname !== "/dns-query") {
+    return Response.redirect("https://baidu.com/", 301)
   }
 
-  if (request.method !== "GET" && request.method !== "POST") {
-    return new Response(`Method ${request.method} not allowed.`, { status: 405 })
-  }
+  //if (request.method !== "GET" && request.method !== "POST") {
+  //  return new Response(`Method ${request.method} not allowed.`, { status: 405 })
+  //}
 
+  if(request.headers.get('content-type') !== 'application/dns-message') {
+        return Response.redirect("https://www.bing.com/", 301)
+  }
   const newURL = `https://${DOH_ADDRESS}${pathname}${search}`
   const newRequest = new Request(newURL, {
     body: request.body,
